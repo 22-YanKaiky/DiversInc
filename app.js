97143bf9b0fef2af4baec1ab447a46e4cf21bf73
22-YanKaiky <yankaikys@gmail.com>
2022-04-09 02:08:33 -0300

    feat(dotenv): require

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+require("dotenv").config();
+
 const app = express();
+const port = process.env.PORT
 
 app.use(cors());
 app.use(express.json());
 
-require("dotenv").config();
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.listen(5000, () => console.log(`server port ${5000}`));
+app.listen(port, () => console.log(`server port ${port}`));
