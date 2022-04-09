bdd39dfe3c0752ab05516cf4879d860bd77fedc7
22-YanKaiky <yankaikys@gmail.com>
2022-04-09 02:41:41 -0300

    feat(routes): use

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const routes = require('./src/routes');
 
 require("dotenv").config();
 
@@ -12,5 +13,7 @@ app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use("/", routes);
 
 app.listen(port, () => console.log(`server port ${port}`));
---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+const users = require('../../data/users.json');
+const createError = require("http-errors");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (_, response) =>
+  response.status(200).json({ message: "© 2022, Divers/nc" })
+);
+
+router.use("/users", (_, response) => {
+  response.status(200).json(users)
+});
+
+router.use(async (_, __, message) => {
+  message(createError.NotFound("Route not Found"));
+});
+
+router.use((error, _, response, __) => {
+  response.status(error.status || 500).json({
+    message: error.message,
+  });
+});
+
+module.exports = router;
